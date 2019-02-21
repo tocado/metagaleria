fb4bf0350daf6697391749753911cc8ecdfb87e8
ToCaDo157 <tocado.157@gmail.com>
2019-02-21 00:11:33 -0300

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-var db = sqlitePlugin.openDatabase({
-    name: 'MetaGaleria.db',
-    location: 'default',
-    androidDatabaseProvider: 'system'
-});
-
-db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS imagen (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, uri TEXT, nombre TEXT, hash TEXT);');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS metadato (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, id_imagen INTEGER, metadato TEXT);');
-}, function(error) {
-    console.log('Transaction ERROR: ' + error.message);
-}, function() {
-    console.log('Populated database OK');
-    alert("base de datos creada");
-});
-
 document.addEventListener('deviceready', function()
 {
     var $content = document.getElementById("content");
